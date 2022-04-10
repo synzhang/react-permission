57ff50fdff5a065852b8cf23abda6988ba796f5f
Syn Zhang <zeyanzhang.china@gmail.com>
2022-04-10 23:45:12 +0800

    chore: config to build commonjs version module

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,33 @@
-import { babel } from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import pkg from './package.json';
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import pkg from "./package.json";
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
 const config = {
-  input: 'src/index.tsx',
-  output: {
-    file: pkg.module,
-    format: 'esm'
-  },
-  external: ['react'],
+  input: "src/index.tsx",
+  output: [
+    {
+      file: pkg.module,
+      format: "esm",
+      sourcemap: "inline",
+    },
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: "inline",
+    },
+  ],
+  external: ["react"],
   plugins: [
     resolve({ extensions }),
     commonjs(),
     babel({
       extensions,
-      babelHelpers: 'bundled',
-      include: ['src/**/*'],
+      babelHelpers: "bundled",
+      include: ["src/**/*"],
     }),
-  ]
+  ],
 };
 
 export default config;
